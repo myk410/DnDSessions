97001a604384584a16afe21bf5b568e4c80a3183
myk410 <myk410@gmail.com>
2021-12-16 11:40:33 -0500

    Initial Commit

---
+++ DnDSessions/DnDSessionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DnDSessionsApp.swift
+//  DnDSessions
+//
+//  Created by G. Michael Fortin Jr on 12/16/21.
+//
+
+import SwiftUI
+
+@main
+struct DnDSessionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
